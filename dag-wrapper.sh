42a9550d4cc8193567e78ac879014724832cc795
Will Maier <wcmaier@hep.wisc.edu>
2011-08-25 07:15:10 -0500

    Add support for DAGMan workflows.
    DAGMan[0] organizes a user's jobs into a graph of nodes. farmout can be run
    several times with the new --output-dag-file and --last-submit-dir options in
    order to build the graph. DAGMan will then manage the jobs in the graph,
    submitting (and retrying) them as necessary. After the DAG terminates, a
    notification email will be sent to the user.
    
    While this approach has clear advantages for multi-step workflows, the
    notification feature may also be useful when submitting a single batch of jobs.
    
    Thanks to Jeff Klukas <jklukas@hep.wisc.edu> for testing this feature!
    
    The full development history of this patch can be found in
    /wcmaier/farmout-dag[1].
    
    
    [0] http://www.cs.wisc.edu/condor/manual/v7.6/2_10DAGMan_Applications.html
    [1] http://hg.hep.wisc.edu/wcmaier/farmout-dag/rev/dag

---
+++ dag-wrapper.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+JOBID=$1
+RETURN=$2
+shift 2
+
+# The rest of the arguments are name=value pairs.
+export $*
+
+cat <<EOF | mail -s "farmout DAG returned ${RETURN}" ${USER}
+Hello-
+
+The following farmout DAG workflow on $(hostname) returned ${RETURN}:
+    
+    ${OUTPUT_DAG_FILE}
+
+You can find the final output in:
+
+    ${OUTPUT_DIR}
+
+If your DAG workflow exited with a status other than 0, you can resubmit only
+the jobs that failed (and their dependencies) with the following command:
+    
+    \$ condor_submit_dag ${OUTPUT_DAG_FILE}
+
+If some of the failed jobs are unrecoverable, you'll need to edit the DAG
+workflow file and mark the unrecoverable jobs 'NOOP'. If the jobs that depend on
+the unrecoverable job require its output, you may have to modify the dependent
+jobs' submit files as well.
+
+For more information about DAG workflows, please see the Condor manual:
+    
+    http://www.cs.wisc.edu/condor/manual/v7.6/2_10DAGMan_Applications.html
+
+For questions about farmout and its DAG support or for help debugging your jobs,
+please contact help@hep.wisc.edu.
+
+Thanks!
+EOF
+
+exit ${RETURN}
