7554a62668fc6e93965af602bb7c2d39e293c78f
Will Maier <wcmaier@hep.wisc.edu>
2011-09-29 15:11:46 -0500

    Include a list of .err files in the DAG notification mail.
    Requested by jklukas@.

--- dag-wrapper.sh
+++ dag-wrapper.sh
@@ -7,7 +7,10 @@ shift 2
 # The rest of the arguments are name=value pairs.
 export $*
 
-cat <<EOF | mail -s "farmout DAG returned ${RETURN}" ${USER}
+tmpdir=$(mktemp -d "farmout-dag-mail-XXXXXXXX")
+trap "rm -rf $tmpdir" ERR EXIT
+
+cat <<EOF > "$tmpdir/mail"
 Hello-
 
 The following farmout DAG workflow on $(hostname) returned ${RETURN}:
@@ -21,6 +24,9 @@ You can find the final output in:
 The status of all of the jobs in this workflow can be found here:
 
     ${OUTPUT_DAG_FILE}.status
+
+A list of files with the error output of your failed jobs can be found at the
+bottom of this message.
 
 If your DAG workflow exited with a status other than 0, you can resubmit only
 the jobs that failed (and their dependencies) with the following command:
@@ -41,5 +47,20 @@ please contact help@hep.wisc.edu.
 
 Thanks!
 EOF
+
+pattern=$(sed -nre '/STATUS_ERROR/!d; s/^JOB (.*) STATUS_.*/\1/p' < "${OUTPUT_DAG_FILE}.status" | tr '\n' '|')
+pattern="($pattern)"
+sed -nre "s;^JOB $pattern (.*)/submit;\2/\1.err;p" < "${OUTPUT_DAG_FILE}" > "$tmpdir/errs"
+
+if [ -s "$tmpdir/errs" ]; then
+    (
+    echo
+    echo "The following files contain the error output for your failed jobs:
+    echo 
+    cat "$tmpdir/errs"
+    ) >> "$tmpdir/mail"
+fi
+
+mail -s "farmout DAG returned ${RETURN}" ${USER} < "$tmpdir/mail"
 
 exit ${RETURN}
